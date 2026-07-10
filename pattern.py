796f14614ecd17330241ce15308a27b1146b7769
meetpatel6424 <meet53814@gmail.com>
2026-07-11 03:42:46 +0530

    patterns

--- pattern.py
+++ pattern.py
@@ -77,8 +77,64 @@
 
 # (number pattern)
 # Pattern 6
+# row = int(input("Enter the number of rows:"))
+# for i in range(0,row):
+#     for j in range(0,i+1):
+#         print(j+1, end=" ")
+#     print()
+#     output:
+#                 1 
+#                 1 2 
+#                 1 2 3 
+#                 1 2 3 4 
+#                 1 2 3 4 5 
+
+# Pattern 7
+# row = int(input("Enter the number of rows:"))
+# for i in range(0,row):
+#     print("  "*(row-i-1), end=" ")
+#     for j in range(0,i+1):
+#         print(j+1, end=" ")
+#     print()
+#     output:
+#                            1 
+#                          1 2 
+#                        1 2 3 
+#                      1 2 3 4 
+#                    1 2 3 4 5 
+
+# Pattern 8
+# row = int(input("Enter the number of rows:"))
+# for i in range(0,row):
+#     print("  "*(i), end=" ")
+#     for j in range(1,row-i+1):
+#         print(j, end=" ")
+#     print()
+#     output:
+#                 1 2 3 4 5 
+#                   1 2 3 4 
+#                     1 2 3 
+#                       1 2 
+#                         1
+
+# Pattern 9
 row = int(input("Enter the number of rows:"))
 for i in range(0,row):
-    for j in range(0,i+1):
+    for j in range(0,row-i):
         print(j+1, end=" ")
     print()
+
+
+# Pattern 10
+# row = int(input("Enter the number of rows:"))
+# for i in range(0,row):
+#     print(" "*(row-i-1), end=" ")
+#     for j in range(0,i+1):
+#         print(j+1, end=" ")
+#     print()
+#     output:
+#                     1 
+#                    1 2 
+#                   1 2 3 
+#                  1 2 3 4 
+#                 1 2 3 4 5 
